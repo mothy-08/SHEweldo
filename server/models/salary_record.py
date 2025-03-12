f234d07197ed4e836d2d41f229c3a94ccbb04b90
Deogenes Maranan <123554998+DeogenesMaranan@users.noreply.github.com>
2025-03-12 22:42:35 +0800

    Add access modifier for salary record model

---
+++ server/models/salary_record.py
@@ -0,0 +1,11 @@
+class SalaryRecord:
+    def __init__(self, company, years_at_company, total_experience, salary_amount, gender, submission_date, is_well_compensated, department, job_title):
+        self.__company = company
+        self.__years_at_company = years_at_company
+        self.__total_experience = total_experience
+        self.__salary_amount = salary_amount
+        self.__gender = gender
+        self.__submission_date = submission_date
+        self.__is_well_compensated = is_well_compensated
+        self.__department = department
+        self.__job_title = job_title
